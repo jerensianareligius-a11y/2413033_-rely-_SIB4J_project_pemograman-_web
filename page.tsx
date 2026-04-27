576cf20c79897e573d1144bf560b9892d7c0e279
jerensianareligius-a11y <jerensianareligius@gmail.com>
2026-04-27 23:14:22 +0700

    learn

---
+++ page.tsx
@@ -0,0 +1,14 @@
+export default function learnpage() {
+    return (
+        <div>
+            <div>
+                <img></img>
+                <p>desaign</p>
+            </div>
+            <div>
+                <h1>figma</h1>
+                <p>8 hours ago</p>
+            </div>
+        </div>
+    );
+}
