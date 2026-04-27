7f30fe051adb14bb0010dab9b208bfb2206d9154
jerensianareligius-a11y <jerensianareligius@gmail.com>
2026-04-27 23:11:23 +0700

    code for visual studio code named blog

---
+++ page.tsx
@@ -0,0 +1,11 @@
+export default function blog () {
+    return (
+        <div>
+            <p>ini adalah halaman blog!</p>
+            <input type="text" placeholder="masukan teks disini" />
+            <button>click me</button>
+        </div>
+    );
+
+}
+
